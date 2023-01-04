c235877a20534d9112b9bd6d5a6e1463380888ce
Hd-Lg <59037834+Hd-Lg@users.noreply.github.com>
2023-01-04 01:58:16 +0100

    Done backend

---
+++ server/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+// Middleware - To allow server to be called from front-end
+app.use(cors());
+// To pass JSON from front-end to back-end
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+	res.status(200).send({
+		message: "Hello there",
+	});
+});
+
+// To have a payload for our request - 'get' is limited on the body we can pass.
+app.post("/", async (req, res) => {
+	try {
+		const prompt = req.body.prompt;
+		const respone = await openai.createCompletion({
+			model: "text-davinci-003",
+			prompt: `${prompt}`,
+			temperature: 0,
+			max_tokens: 3000,
+			top_p: 1,
+			frequency_penalty: 0.5,
+			presence_penalty: 0,
+		});
+		res.status(200).send({
+			bot: respone.data.choices[0].text,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({ error });
+	}
+});
+
+app.listen(5000, () =>
+	console.log("Server is running on port http://localhost:5000")
+);
